221012a3f1c7bd6686edabe76c1beef654c3bc44
Allen Wu <menglunwu@gmail.com>
2025-10-21 13:13:29 +0800

    Initial Commit

---
+++ bopomofo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  bopomofo
+//
+//  Created by ALLEN WU on 10/21/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ bopomofoTests/bopomofoTests.swift
@@ -0,0 +1,16 @@
+//
+//  bopomofoTests.swift
+//  bopomofoTests
+//
+//  Created by ALLEN WU on 10/21/25.
+//
+
+import Testing
+
+struct bopomofoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
